d9850d952681f5a399061cc4242c8be7fbee1bf6
Sara <saradegra@gmail.com>
2022-12-01 19:48:57 +0100

    bodyParser for POST requests

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3005;
 
 app.use(cors({
-    origin: '*'
+    origin: '*' //'http://localhost:XXXX' indirizzo flutter
 }));
+
+//the encoding of the request (post) Contet-Type: application/json
+app.use(bodyParser.json())
 
 app.get('/',(req,res) => {
     console.log("prova in json")
@@ -15,10 +19,36 @@ app.get('/',(req,res) => {
 app.get('/check-login',(req,res) => {
     //TODO query al db
     //controlla che esistano nome e pwd
-    //dovrebbe diventare una post
-    console.log("check-login")
+    //TODO fare una post 
+    console.log("GET check-login")
     res.status(200).json({isUser:'true'});
 })
 
+app.post('/prova-post', (req, res) => {
+    console.log("POST feedback")
+    res.status(200).json({feedback:'true'})
+})
+
+app.get('/lista', (req, res) => {
+    console.log("GET json cibo")
+    var cibo = {
+        noodles: "pollo",
+        pizza: "margherita",
+        panettone: "esotico"
+    }
+
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200)
+    res.json(cibo)
+})
+
+app.get('/select', (req, res) => {
+    console.log("GET select regali")
+    let dati = ""
+    //TODO query = "SELECT * FROM regali"
+    res.setHeader('Content-Type', 'application/json')
+    res.status(200)
+    res.json(dati)
+})
 app.listen(port, () => console.log('Server in ascolto'));
 
--- readme.txt
+++ readme.txt
@@ -5,9 +5,13 @@ per attivare:
 node index.js
 
 per installare:
-npm install --save express
-npm install --save cors
+npm install express
+npm install cors: for enabling CORS
+npm install body-parser: Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
 
 per uscire:
 control + C
 oppure .exit
+
+per aggiornare: 
+spegnere e riaccendere il server
