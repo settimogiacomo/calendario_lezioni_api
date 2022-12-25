9f43ce929be900b934107debf644e8dac14feb48
settimogiacomo <settimogiacomo.98@gmail.com>
2022-12-26 00:10:50 +0100

    Get calendario settimana
    fuma che anduma

--- costanti.js
+++ costanti.js
@@ -1,6 +1,6 @@
 module.exports = function () {
     this.HOST = 'localhost'
     this.USER = 'root'
-    this.PASS = 'root' // root
+    this.PASS = '' // root
     this.NOME_DB = 'calendario_lezioni'
 } 
--- index.js
+++ index.js
@@ -104,10 +104,54 @@ app.get('/get-lezioni',(req,res) => {
     
 })
 
+app.get('/lezione/:materia',(req,res) => {
+
+    try{
+        conn.query('SELECT * FROM calendario_settimana1 WHERE materia = "' + req.params.materia + '" AND stato =  "2"', (err, rows, fields) => {
+            //if (err) throw err
+            if (err){
+                res.json({ isTable: 'false' })
+            } else {
+               
+                if(rows.length >= 1){
+                    var result = [];
+                        for (let i=0;i<rows.length;i++) {
+                            //parsing datetime
+                           // console.log(rows[i].giorno);
+                            console.log(rows[i].inizio_lezione)
+                            console.log(rows[i].fine_lezione)
+
+                           // moment.locale('it')
+                            //let inizioLezione = moment(rows[i].inizio_lezione).utcOffset(60).format('DD/MM/YYYY, hh:mm A') 
+                            //let fineLezione = moment(rows[i].fine_lezione).utcOffset(60).format('DD/MM/YYYY, hh:mm A')
+
+                            result.push({ id_giorno: rows[i].id_giorno, inizio_lezione: rows[i].inizio_lezione, fine_lezione: rows[i].fine_lezione})
+                        }
+                        console.log(result)
+                        res.json({isTable:'true', data:result })
+                        
+                } else {
+                    res.json({ isTable: 'false' })
+                }
+                
+            }
+            
+        })
+    } catch(errore) {
+        res.json ({isTable:'false', debug:errore})
+    }
+    
+   
+    
+})
+
+
 app.post('/prova-post', (req, res) => {
     console.log("POST feedback")
     res.status(200).json({feedback:'true'})
 })
+
+
 
 app.get('/lista', (req, res) => {
     console.log("GET json cibo")
