c221ae25446dd66400a8a687959b7903e153e10b
Sara <saradegra@gmail.com>
2023-01-04 15:07:21 +0100

    Endpoint lezoine effettuata

--- index.js
+++ index.js
@@ -63,11 +63,12 @@ app.post('/check-login',(req,res) => {
 })
 
 
-app.get('/get-lezioni',(req,res) => {
+app.get('/get-lezioni/:id_studente',(req,res) => {
     var body = req.body
 
     try{
-        conn.query('SELECT * FROM lista_lezioni_vista', (err, rows, fields) => {
+        conn.query('SELECT * FROM lista_lezioni_vista WHERE id_studente = "' + req.params.id_studente + '"', (err, rows, fields) => {
+            
             //if (err) throw err
             if (err){
                 res.json({ isTable: 'false' })
@@ -108,7 +109,7 @@ app.get('/get-lezioni',(req,res) => {
 app.get('/lezione/:materia',(req,res) => {
 
     try{
-        conn.query('SELECT cs.id_giorno, cs.id_insegnante, im.insegnante, cs.materia, cs.inizio_lezione, cs.fine_lezione, cs.stato FROM calendario_settimana cs JOIN insegnante_materia im ON (im.id_insegnante = cs.id_insegnante) WHERE cs.materia = "' + req.params.materia + '" AND cs.stato =  "0"', (err, rows, fields) => {
+        conn.query('SELECT cs.cod_lezione, cs.id_giorno, cs.id_insegnante, im.insegnante, cs.materia, cs.inizio_lezione, cs.fine_lezione, cs.stato FROM calendario_settimana cs JOIN insegnante_materia im ON (im.id_insegnante = cs.id_insegnante) WHERE cs.materia = "' + req.params.materia + '" AND cs.stato =  "0"', (err, rows, fields) => {
             //if (err) throw err
             if (err){
                 res.json({ isTable: 'false' })
@@ -117,17 +118,8 @@ app.get('/lezione/:materia',(req,res) => {
                 if(rows.length >= 1){
                     var result = [];
                         for (let i=0;i<rows.length;i++) {
-                            //parsing datetime
-                           // console.log(rows[i].giorno);
-                           
-                            console.log(rows[i].inizio_lezione)
-                            console.log(rows[i].fine_lezione)
 
-                           // moment.locale('it')
-                            //let inizioLezione = moment(rows[i].inizio_lezione).utcOffset(60).format('DD/MM/YYYY, hh:mm A') 
-                            //let fineLezione = moment(rows[i].fine_lezione).utcOffset(60).format('DD/MM/YYYY, hh:mm A')
-
-                            result.push({ id_giorno: rows[i].id_giorno, id_insegnante: rows[i].id_insegnante, insegnante: rows[i].insegnante, materia: rows[i].materia, inizio_lezione: rows[i].inizio_lezione, fine_lezione: rows[i].fine_lezione, stato: rows[i].stato})
+                            result.push({ cod_lezione: rows[i].cod_lezione, id_giorno: rows[i].id_giorno, id_insegnante: rows[i].id_insegnante, insegnante: rows[i].insegnante, materia: rows[i].materia, inizio_lezione: rows[i].inizio_lezione, fine_lezione: rows[i].fine_lezione, stato: rows[i].stato})
                         }
                         console.log(result)
                         res.json({isTable:'true', data:result })
@@ -154,7 +146,7 @@ app.post('/prenota', (req, res) => {
     moment.locale('it')
     var giorno = moment(day).utcOffset(60).format('YYYY-MM-DD') 
 
-    var query = "INSERT INTO lista_lezioni (id_insegnante, id_studente, inizio_lezione, fine_lezione, stato) VALUES ('" + body.id_insegnante + "', '" + body.id_studente + "', '" + giorno + " " + body.inizio_lezione + "', '" + giorno + " " + body.fine_lezione + "', '1')"
+    var query = "INSERT INTO lista_lezioni (id_insegnante, id_studente, inizio_lezione, fine_lezione, stato, cod_lezione) VALUES ('" + body.id_insegnante + "', '" + body.id_studente + "', '" + giorno + " " + body.inizio_lezione + "', '" + giorno + " " + body.fine_lezione + "', '1', '" + body.cod_lezione + "')"
   
 
     try {
@@ -165,7 +157,7 @@ app.post('/prenota', (req, res) => {
             } else {
               //res.json({ ok: 'true' })
 
-              var query2 = "UPDATE calendario_settimana SET stato = 1 WHERE  id_giorno = '" + body.id_giorno + "' AND id_insegnante = '" + body.id_insegnante+"' AND inizio_lezione  = '" + body.inizio_lezione +"' "
+                var query2 = "UPDATE calendario_settimana SET stato = 1 WHERE cod_lezione = '" + body.cod_lezione + "'"
 
               conn.query(query2, (err, rows, fields) => {
                 if (err) {
@@ -173,7 +165,7 @@ app.post('/prenota', (req, res) => {
                 } else {
                     res.json({ ok: 'true' })
                 }
-            })
+              })
             }
 
         })
@@ -184,35 +176,36 @@ app.post('/prenota', (req, res) => {
 
 })
 
+app.get('/effettuata/:id_lezione', (req, res) => {
+console.log("effettuataa " + req.params.id_lezione)
+    try {
+        conn.query('UPDATE lista_lezioni SET stato = 2 WHERE id_lezione = ' + req.params.id_lezione, (err, rows, fields) => {
+            //if (err) throw err
+            if (err) {
+                res.json({ ok: 'false' })
+            } else {
 
-app.post('/prova-post', (req, res) => {
-    console.log("POST feedback")
-    res.status(200).json({feedback:'true'})
-})
+                var query2 = "UPDATE calendario_settimana SET stato = 2 WHERE id_lezione = " + req.params.id_lezione
 
+                conn.query(query2, (err, rows, fields) => {
+                    if (err) {
+                        res.json({ ok: 'false', debug: err })
+                    } else {
+                        res.json({ ok: 'true' })
+                    }
 
+                })
 
-app.get('/lista', (req, res) => {
-    console.log("GET json cibo")
-    var cibo = {
-        noodles: "pollo",
-        pizza: "margherita",
-        panettone: "esotico"
+            }
+
+        })
+    } catch (errore) {
+        res.json({ ok: 'false', debug: errore })
     }
 
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200)
-    res.json(cibo)
 })
 
-app.get('/select', (req, res) => {
-    console.log("GET select regali")
-    let dati = ""
-    //TODO query = "SELECT * FROM regali"
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200)
-    res.json(dati)
-})
+
 
 app.listen(port, () => console.log('Server in ascolto'));
 
